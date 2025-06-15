9836136caffa682367e1e71ce156dc9b11aac7fe
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-15 21:35:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function firstChar(text) {
   // your code here
-	if (text === "" || text === " ") {
-		return '';
-	}
-	return text.trim()[0];
+	const trimmedText = text.trim();
+	return trimmedText.charAt(0);
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 const text = prompt("Enter text:");
+
 alert(firstChar(text));
