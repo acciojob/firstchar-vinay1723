19cc0ec390866cd71c65cc7e10dec7cd6c6fb749
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-15 21:27:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstChar(text) {
   // your code here
+	return text.trim()[0];
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
